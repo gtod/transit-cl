e85d6f2de913f921d734ea914b849ae788c32dce
Gregory Tod <vlad@gtod.net>
2015-04-02 14:24:56 +1100

    Fixes transit-format issue 21, recursive map keys
    See https://github.com/cognitect/transit-format/issues/21
    
    By making parse-transit% accept a key-p arg and calling it recusrively
    within array parses.
    
    Remove now obsolete object-key-reader.
    
    Change remaining yason:parse calls to explict parse-transit% calls (they
    were bound there anyway).

--- t/implementation.lisp
+++ t/implementation.lisp
@@ -5,3 +5,8 @@
 
 (5am:test composite-tagged-p
    (5am:finishes (composite-tagged-p #())))
+
+;; Fixes https://github.com/cognitect/transit-format/issues/21
+(5am:test recursive-key-p
+  (5am:is (equal? (map ("cached" 0) ('explain "cached"))
+                  (transit-read "[\"^ \",[\"~#'\",\"cached\"],0,\"~$EXPLAIN\",\"^0\"]"))))
