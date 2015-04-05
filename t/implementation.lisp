b15ea2e4732426c023c2bb62732f2e34ea80e8df
Gregory Tod <vlad@gtod.net>
2015-04-05 19:48:09 +1000

    Simplify implementation test suite decls

--- t/implementation.lisp
+++ t/implementation.lisp
@@ -1,7 +1,7 @@
 (in-package :transit-cl)
 
-(def-suite :implementation :in :transit-cl)
-(in-suite :implementation)
+(5am:def-suite implementation)
+(5am:in-suite implementation)
 
 (5am:test composite-tagged-p
    (5am:finishes (composite-tagged-p #())))
