8507810f315dfb74331ecd90daf59d5f68942f65
Gregory Tod <vlad@gtod.net>
2015-04-05 19:48:58 +1000

    Make a transit-cl-test package

--- t/package.lisp
+++ t/package.lisp
@@ -1,6 +1,9 @@
 (in-package :transit-cl)
 
-(use-package :fiveam)
+(defpackage :transit-cl-test
+  (:use :cl :transit-cl :fiveam :local-time)
+  (:import-from :fset #:equal? #:size #:empty-map)
+  (:shadowing-import-from :fset #:map #:set))
 
 (defpackage :transit-cl-exemplars
   (:use :cl :transit-cl :fiveam :named-readtables :alexandria :cl-ppcre
